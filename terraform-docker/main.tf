03fd33908c33c52f4ab26904011bb467d400618f
Martin Le <martin.minh.le@gmail.com>
2024-06-25 21:59:05 +1000

    create multiple resources using splat operator

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -9,3 +9,33 @@ terraform {
 
 provider "docker" {}
 
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest-minimal"
+}
+
+resource "random_string" "random" {
+  count   = 1
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "docker_container" "nodered_container" {
+  count = 1
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+  }
+}
+
+output "ip-address" {
+  value = [for i in docker_container.nodered_container[*]: join(":", [i.network_data[0].ip_address], i.ports[*]["external"])]
+  description = "The IP address and external port of the container"
+}
+
+output "container-name" {
+  value       = docker_container.nodered_container[*].name
+  description = "The name of the container"
+}
+
